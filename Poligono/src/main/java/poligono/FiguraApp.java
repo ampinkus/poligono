fa8d144b26be39be77e1b761704278ab4d609bdf
ampinkus <ampinkus@gmail.com>
2022-07-18 17:35:28 -0300

    Subida inicial

--- Poligono/src/main/java/poligono/FiguraApp.java
+++ Poligono/src/main/java/poligono/FiguraApp.java
@@ -2,23 +2,38 @@ package poligono;
 
 import java.util.Scanner;
 public class FiguraApp {
+
+
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);  // creo el objeto entrada para escanear el teclado
         int elementos;  // numero de puntos del poligono
-        Double X, Y; // coordenadas de los puntos  
+        Double X, Y; // coordenadas de los puntos
 
-        System.out.print("\nIngrese la cantidad de puntos del polígono: ");
+        System.out.print("\nIngrese la cantidad de puntos del poligono: ");
         elementos = entrada.nextInt(); // ingreso un entero: variable = entrada.nextInt()
-        Poligono poligono = new Poligono(elementos); // creo un poligono con el numero de puntos = elementos
+        Punto[] puntos = new Punto[elementos]; // creo un arreglo de puntos para crear el polígono
+        Poligono poligono = new Poligono(elementos); // creo un poligono con un numero elementos de puntos
 
-        System.out.print("\nIngrese las coordenadas del poligono en sentido horario: ");
+        System.out.println("Ingrese las coordenadas del poligono en sentido horario: ");
         for (int i = 0; i < elementos; i++) {
-            System.out.print("\nIngrese la coordenada X  del punto " + (i + 1) + " :" );
-            int elementos = entrada.nextInt(); // ingreso un entero: variable = entrada.nextInt()
-
-
+            System.out.print("Ingrese la coordenada X  del punto " + (i + 1) + " : " );
+            X = entrada.nextDouble(); // ingreso un Double: variable = entrada.nextDouble()
+            System.out.print("Ingrese la coordenada Y  del punto " + (i + 1) + " : " );
+            Y = entrada.nextDouble(); // ingreso un Double: variable = entrada.nextDouble()
+            System.out.println();
+            puntos[i] = new Punto(X,Y); // ingreso en el punto de indice i los valores de X e Y
+            poligono.Insertar(i,puntos[i]);  // ingreso en el indice i del poligono el punto
         }
 
+        for (int i = 0; i < elementos; i++) {  // muestro las coordenadas dee los puntos del poligono
+            System.out.print("Coordenada X  del punto " + (i + 1) + " del poligono: " );
+            poligono.MostrarX(i);
+            System.out.print("Coordenada Y  del punto " + (i + 1) + " del poligono: " );
+            poligono.MostrarY(i);
+            System.out.println();
+        }
+
+        System.out.println("El area del poligono es: " + poligono.areaPoligono(poligono,elementos));
 
     }
 
--- Poligono/src/main/java/poligono/Poligono.java
+++ Poligono/src/main/java/poligono/Poligono.java
@@ -16,22 +16,71 @@ public class Poligono extends Figura {
     // pongo un punto de la clase Punto en el poligono en la posición dada por posicion
     // Uso:  <nombre del poligono>(posicion, punto)
     // posicion es un entero y punto es un objeto de la clase Punto
-    void Insertar(int posicion, Punto punto) {  // pongo un punto de la clase Punto en la posición dada por posicion
+    public void Insertar(int posicion, Punto punto) {  // pongo un punto de la clase Punto en la posición dada por posicion
         poligono[posicion] = punto;
     }
 
     // guarda la coordenada X del punto que esta en el indice posición del poligono
     // Uso: <nombre del poligono>.guardarPuntoX(posicion, x)
     // posicion es entero y x es Double
-    void guardarPuntoX(int posicion, Double x) { // retorna la coordenada X del punto que esta en el indice posición
+    public void guardarPuntoX(int posicion, Double x) { // retorna la coordenada X del punto que esta en el indice posición
         poligono[posicion].setX(x);
     }
 
     // guarda la coordenada Y del punto que esta en el indice posición del poligono
     // Uso: <nombre del poligono>.guardarPuntoY(posicion, y)
     // posicion es entero e y  es Double
-    void guardarPuntoY(int posicion, Double y) {
+    public void guardarPuntoY(int posicion, Double y) {
         poligono[posicion].setY(y);
     }
+
+    // Muestro el valor de la coordenada X del punto en el indice posicion del polígono
+    // Uso: <nombre del poligono>.MostrarX(posicion)
+    // posicion es un entero
+    public void MostrarX(int posicion) {
+        // poligono[posicion] me retorna el punto y le aplico getX() al mismo
+        System.out.println(poligono[posicion].getX());
+    }
+
+    // Muestro el valor de la coordenada Y del punto en el indice posicion del polígono
+    // Uso: <nombre del poligono>.MostrarY(posicion)
+    // posicion es un entero
+    public void MostrarY(int posicion) {
+        // poligono[posicion] me retorna el punto y le aplico getY() al mismo
+        System.out.println(poligono[posicion].getY());
+    }
+
+    // retorna la coordenada X del punto que esta en el indice posición del poligono
+    // Uso: <nombre del poligono>.retornarPuntoX(posicion)
+    // posicion es un entero
+    public Double retornarPuntoX(int posicion) { // retorna la coordenada X del punto que esta en el indice posición
+        return poligono[posicion].getX();
+    }
+
+    // retorna la coordenada Y del punto que esta en el indice posición del poligono
+    // Uso: <nombre del poligono>.retornarPuntoY(posicion)
+    // posicion es un entero
+    public Double retornarPuntoY(int posicion) {
+        return poligono[posicion].getY();
+    }
+
+
+    // Calculamos el valor del area utilizando la formula de "lazo de zapato"
+    // Uso:  Los argumentos son el poligono del cual queremos calcular el área y el número de vertices
+    public double areaPoligono(Poligono poligono, int n)
+    {
+        // inicializamos el  area
+        Double area = 0D;
+        int j = n - 1;
+        for (int i = 0; i < n; i++) {
+            // area += (X[j] + X[i]) * (Y[j] - Y[i]);
+            area += (poligono.retornarPuntoX(j)  + poligono.retornarPuntoX(i)) * (poligono.retornarPuntoY(j) - poligono.retornarPuntoY(i));
+            // agregamos el ultimo vertice nuevamente
+            j = i;
+        }
+        // Retornamos el valor absoluto del área
+        return Math.abs(area / 2.0);
+    }
+
 
 }
