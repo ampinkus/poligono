1d7746a80487875fc982210235cbf4889b98a34e
ampinkus <ampinkus@gmail.com>
2022-07-19 14:18:05 -0300

    Agregado el test de circunferencia

--- Poligono/src/test/java/poligono/PoligonoTest.java
+++ Poligono/src/test/java/poligono/PoligonoTest.java
@@ -4,9 +4,8 @@ import org.junit.Test; // tengo que importar esta clase para poder usar @Test
 import static org.junit.jupiter.api.Assertions.*; // tengo que importar esta clase para podr usar assert
 
 public class PoligonoTest {
-
     @Test
-    public void test_area_circunferencia(){
+    public void test_area_circunferencia_pasa(){
         // given: Donde ponemos los datos
         Double radio = 2D;
         Double area;
@@ -16,9 +15,22 @@ public class PoligonoTest {
         area = poligono.areaPoligono(radio);
 
         // then: Donde chequeamos el resultado
-        // si el radio es 2 el área es PI * 4, con un delta de 2 y un radio de 1.9 da positivo
+        // si el radio es 2 el área es PI * 4
         assertEquals(Math.PI * 4, area,0.5);
-
     }
 
+    @Test
+    public void test_area_circunferencia_no_pasa(){
+        // given: Donde ponemos los datos
+        Double radio = 2D;
+        Double area;
+        Poligono poligono = new Poligono(1);
+
+        //when: Donde ejecutamos el método a probar
+        area = poligono.areaPoligono(radio);
+
+        // then: Donde chequeamos el resultado
+        // si el radio es 2 el área es PI * 4, en este caso debe no debe pasar el test
+        assertEquals(Math.PI * 3, area,0.5);
+    }
 }
