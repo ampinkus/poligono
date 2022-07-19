efe68de92cb22150b593b692c37c437bb696fd20
ampinkus <ampinkus@gmail.com>
2022-07-19 14:10:14 -0300

    Agregado el test de circunferencia

--- Poligono/src/main/java/poligono/Figura.java
+++ Poligono/src/main/java/poligono/Figura.java
@@ -3,12 +3,11 @@ public abstract class Figura {
     // clase abstracta donde defino dos métodos para calcular el area:
     // uno para una circunferencia y otro para un poligono.
 
-
     // Calculamos el valor del area de un poligono utilizando la formula de "lazo de zapato"
-    // como es un metodo abstracto solo va la firma del mismo
+    // como es un metodo abstracto solo va la firma del mismo.  Ver que en la firma se define como abstracta!
     public abstract Double areaPoligono(Poligono poligono, int n);
 
     // Calculamos el area de una circunferencia PI * radio ^2
-    // como es un metodo abstracto solo va la firma del mismo
+    // como es un metodo abstracto solo va la firma del mismo. Ver que al ser abtracta el método carece de cuerpo
     public abstract Double areaPoligono(Double radio);
 }
--- Poligono/src/test/java/poligono/PoligonoTest.java
+++ Poligono/src/test/java/poligono/PoligonoTest.java
@@ -1,12 +1,24 @@
 package poligono;
 
-import junit.framework.TestCase;
+import org.junit.Test; // tengo que importar esta clase para poder usar @Test
+import static org.junit.jupiter.api.Assertions.*; // tengo que importar esta clase para podr usar assert
 
-public class PoligonoTest extends TestCase {
+public class PoligonoTest {
 
-    public void testGuardarPuntoX() {
+    @Test
+    public void test_area_circunferencia(){
+        // given: Donde ponemos los datos
+        Double radio = 2D;
+        Double area;
+        Poligono poligono = new Poligono(1);
+
+        //when: Donde ejecutamos el método a probar
+        area = poligono.areaPoligono(radio);
+
+        // then: Donde chequeamos el resultado
+        // si el radio es 2 el área es PI * 4, con un delta de 2 y un radio de 1.9 da positivo
+        assertEquals(Math.PI * 4, area,0.5);
+
     }
 
-    public void testGuardarPuntoY() {
-    }
 }
